509178cd6fb43badecacf35187fd0e3cb8e767fa
meister03 <69507874+meister03@users.noreply.github.com>
2022-09-29 17:05:44 +0200

    export discordeno members

--- src/mod.ts
+++ src/mod.ts
@@ -1,8 +1,6 @@
-// @ts-ignore
 export * from './Client';
 export * from './Server';
 export * from './Providers/mod';
-// @ts-ignore
 export * from './Discordeno.ts/mod';
 // @ts-ignore
 export * from 'discordeno'
